57bae67bdcca3d42dd92c3c84e458fd1257280d3
BlazarQ <134844754+BlazarQ@users.noreply.github.com>
2025-11-30 23:01:45 +0800

    changed timestamp columns to varchar

--- scripts/ingest_script.py
+++ scripts/ingest_script.py
@@ -137,7 +137,6 @@ def map_dtype(dtype):
     if pd.api.types.is_integer_dtype(dtype): return INTEGER
     elif pd.api.types.is_float_dtype(dtype): return FLOAT
     elif pd.api.types.is_bool_dtype(dtype): return BOOLEAN
-    elif pd.api.types.is_datetime64_any_dtype(dtype): return TIMESTAMP
     else: return VARCHAR
 
 # ----------------------------------------------------------
